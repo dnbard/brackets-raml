c23a50c82327b64424a94e68a1019f54653434c1
Oleksandr Bardanov <oleksandr.bardanov@avid.com>
2015-10-30 13:42:12 +0200

    show raml browser in iframe

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
 define(function(require, exports, module){
     var StatusBar = brackets.getModule('widgets/StatusBar'),
+        MainViewManager = brackets.getModule('view/MainViewManager'),
         Dialogs = brackets.getModule('widgets/Dialogs'),
+        ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
         isVisible = true,
-        indicatorId = 'indicator-dnbard.raml',
-        modulePath = brackets.getModule("utils/ExtensionUtils").getModulePath(module),
-        indicatorDOM = $('<div>RAML</div>');
+        indicatorId = 'indicator-dnbard-raml',
+        dialogId = 'dialog-dnbard-raml',
+        modulePath = ExtensionUtils.getModulePath(module),
+        indicatorDOM = $('<div>RAML</div>'),
+        ramlRegex = /\.raml$/i,
+        currentFile = null;
+
+    ExtensionUtils.loadStyleSheet(module, 'styles.css');
 
     StatusBar.addIndicator(indicatorId, indicatorDOM, isVisible);
 
     indicatorDOM.click(function(){
-        Dialogs.showModalDialog('dialog-dnbard.raml', '<iframe src="' + modulePath + 'api-console/index.html?raml=https://raw.githubusercontent.com/mulesoft/api-console/master/dist/examples/box.raml"></iframe>', "", [], true);
+        if (currentFile === null){
+            return console.error('File entity are null');
+        }
+
+        Dialogs.showModalDialog(dialogId, 'RAML', '<iframe src="' + modulePath + 'api-console/index.html"></iframe>', "", [], true);
+
+        window.addEventListener("message", function(event){
+            if (event.data === "RAMLReady"){
+                var ramlIframe = $('.dialog-dnbard-raml iframe')[0].contentWindow;
+                currentFile.read(function(err, fileData){
+                    ramlIframe.postMessage(fileData, '*');
+                });
+            }
+        });
+    });
+
+    function toggleIndicator(isVisibleState){
+        StatusBar.updateIndicator(indicatorId, isVisibleState);
+    }
+
+    MainViewManager.on('currentFileChange', function(event, file){
+        toggleIndicator(!!ramlRegex.test(file._path));
+        currentFile = file;
     });
 });
